38487cb5b007c585203137b65feb6c02a9eceb87
twalk2 <2twalk2@gmail.com>
2019-11-04 11:38:01 -0700

    add to cart and remove cart functionality

--- main.js
+++ main.js
@@ -6,8 +6,37 @@ var app = new Vue({
     image: "./images/green-vue-socks.png",
     // image: "./images/blue-vue-socks.png"
     website: "https://utah-ski-central.herokuapp.com/",
-    inventory: 100,
+    inStock: false,
     onSale: true,
-    details: ["80% cotton", "20% polyester", "Super Durable"]
+    details: ["80% cotton", "20% polyester", "Super Durable"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "./images/green-vue-socks.png"
+      },
+      {
+        variantId: 2234,
+        variantColor: "blue",
+        variantImage: "./images/blue-vue-socks.png"
+      }
+    ],
+    sizes: ["S", "M", "L", "XL"],
+    cart: 0
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (this.cart > 0) {
+        this.cart -= 1;
+      } else {
+        return 0;
+      }
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
